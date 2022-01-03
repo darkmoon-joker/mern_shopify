d929a90e92cc37dbd5cb7febfac543a012b4898a
joker-softdev <jokernthn@gmail.com>
2022-01-03 18:41:32 +0530

    db connected and redux started

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import "./bootstrap.min.css"
+import {Provider} from 'react-redux'
+import store from './store'
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
+ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'))
